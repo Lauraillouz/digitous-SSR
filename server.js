de5766bc7df7a4cd8bd1ddc9cf2be7ad9e0195bc
Laura Illouz <laura.hortense@gmail.com>
2021-10-01 10:22:54 +0200

    login corrected

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 const hbs = require("express-handlebars");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { Pool } = require("pg");
+const Postgres = new Pool({ ssl: { rejectUnauthorized: false } });
+const dotenv = require("dotenv");
+dotenv.config({
+  path: "config.env",
+});
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,8 +18,14 @@ app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(express.static("public"));
 
 app.get("/", (_req, res) => {
+  res.render("home", {});
+});
+
+app.post("/", (req, res) => {
+  const user = req.body;
   res.render("home", {});
 });
 
@@ -23,12 +37,33 @@ app.get("/signup", (req, res) => {
   res.render("signup", {});
 });
 
-app.post("/profile", (req, res) => {
-  const user = req.body;
-  console.log(user);
-  res.render("profile", {
-    username: user.username,
-  });
+app.post("/profile", async (req, res) => {
+  const userInfo = req.body;
+
+  try {
+    // check if user exists
+    const user = await Postgres.query("SELECT * FROM users WHERE email=$1", [
+      userInfo.email,
+    ]);
+
+    if (user.rows.length === 0) {
+      const hashedPassword = await bcrypt.hash(userInfo.password, 12);
+      await Postgres.query(
+        "INSERT INTO users(username, email, password) VALUES($1, $2, $3)",
+        [userInfo.username, userInfo.email, hashedPassword]
+      );
+
+      res.render("profile", {
+        username: userInfo.username,
+      });
+    } else {
+      alert("This email has already been used.");
+    }
+  } catch (err) {
+    res.status(404).json({
+      message: err,
+    });
+  }
 });
 
 app.get("/users", (req, res) => {
